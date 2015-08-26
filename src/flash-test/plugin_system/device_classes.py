8a8cb968dbc7d13c2987bd251c09b15870bbe616
Olaf Lüke <olaf@tinkerforge.com>
2015-08-26 10:34:10 +0200

    Add Load Cell Bricklet plugin and fix typos

--- src/flash-test/plugin_system/device_classes.py
+++ src/flash-test/plugin_system/device_classes.py
@@ -1,6 +1,7 @@
 from .plugins.brick_imu_v2 import Plugin as brick_imu_v2_class
 from .plugins.bricklet_accelerometer import Plugin as bricklet_accelerometer_class
 from .plugins.bricklet_ambient_light_v2 import Plugin as bricklet_ambient_light_v2_class
+from .plugins.bricklet_load_cell import Plugin as bricklet_load_cell_class
 from .plugins.bricklet_moisture import Plugin as bricklet_moisture_class
 from .plugins.bricklet_remote_switch import Plugin as bricklet_remote_switch_class
 from .plugins.bricklet_rs232 import Plugin as bricklet_rs232_class
@@ -9,6 +10,7 @@ device_classes = [
     brick_imu_v2_class,
     bricklet_accelerometer_class,
     bricklet_ambient_light_v2_class,
+    bricklet_load_cell_class,
     bricklet_moisture_class,
     bricklet_remote_switch_class,
     bricklet_rs232_class,
--- src/flash-test/plugin_system/plugins/bricklet_load_cell.py
+++ src/flash-test/plugin_system/plugins/bricklet_load_cell.py
@@ -3,7 +3,7 @@
 flash-test (Brick/Bricklet/Extension Flash and Test tool)
 Copyright (C) 2015 Olaf Lüke <olaf@tinkerforge.com>
 
-bricklet_accelerometer.py: Accelerometer plugin
+bricklet_load_cell.py: Load Cell plugin
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -23,7 +23,7 @@ Boston, MA 02111-1307, USA.
 
 from PyQt4 import Qt, QtGui, QtCore
 
-from ..tinkerforge.bricklet_accelerometer import BrickletAccelerometer
+from ..tinkerforge.bricklet_load_cell import BrickletLoadCell
 from ..bricklet_base import BrickletBase, get_bricklet_firmware_filename
 from ..callback_emulator import CallbackEmulator
 
@@ -31,11 +31,11 @@ import math
 
 class Plugin(BrickletBase):
     TODO_TEXT = u"""\
-1. Verbinde Accelerometer Bricklet mit Port C
+1. Verbinde Load Cell Bricklet mit Port C
 2. Drücke "Flashen"
 3. Warte bis Master Brick neugestartet hat (Tool Status ändert sich wieder auf "Plugin gefunden")
 4. Überprüfe Wert:
-     * Neigung und Drehung müssen zu Bricklet-Position passen
+     * Stecke Bricklet in Testaufbau, überprüfe ob Wert steigt wenn auf Wägezelle gedrückt wird
      * LED muss dabei durchgängig blinken
 5. Das Bricklet ist fertig, in kleine ESD-Tüte stecken, zuschweißen, Aufkleber aufkleben
 6. Gehe zu 1
@@ -43,7 +43,7 @@ class Plugin(BrickletBase):
 
     def __init__(self, *args):
         BrickletBase.__init__(self, *args)
-        self.cbe_acceleration = None
+        self.cbe_weight = None
         self.led_is_on = False
 
     def start(self, device_information):
@@ -53,42 +53,29 @@ class Plugin(BrickletBase):
             self.new_enum(device_information)
 
     def stop(self):
-        if self.cbe_acceleration:
-            self.cbe_acceleration.set_period(0)
+        if self.cbe_weight:
+            self.cbe_weight.set_period(0)
 
     def get_device_identifier(self):
-        return BrickletAccelerometer.DEVICE_IDENTIFIER
+        return BrickletLoadCell.DEVICE_IDENTIFIER
     
     def flash_clicked(self):
         self.write_new_uid_to_bricklet()
-        self.write_plugin_to_bricklet(get_bricklet_firmware_filename('accelerometer'))
+        self.write_plugin_to_bricklet(get_bricklet_firmware_filename('load_cell'))
         self.master_reset()
         
     def new_enum(self, device_information):
-        self.accelerometer = BrickletAccelerometer(device_information.uid, self.get_ipcon())
-        self.cbe_acceleration = CallbackEmulator(self.accelerometer.get_acceleration,
-                                                 self.cb_acceleration)
-        self.cbe_acceleration.set_period(100)
+        self.load_cell = BrickletLoadCell(device_information.uid, self.get_ipcon())
+        self.cbe_weight = CallbackEmulator(self.load_cell.get_weight,
+                                           self.cb_weight)
+        self.cbe_weight.set_period(100)
 
         self.mw.set_tool_status_okay("Plugin gefunden")
             
-    def cb_acceleration(self, data):
-        if self.accelerometer.is_led_on():
-            self.accelerometer.led_off()
+    def cb_weight(self, weight):
+        if self.load_cell.is_led_on():
+            self.load_cell.led_off()
         else:
-            self.accelerometer.led_on()
+            self.load_cell.led_on()
         
-        x, y, z = data
-
-        try:
-            pitch = int(round(math.atan(x/(math.sqrt(y*y + z*z)))*180/math.pi, 0))
-            roll  = int(round(math.atan(y/math.sqrt(x*x+z*z))*180/math.pi, 0))
-        except:
-            pitch = 0
-            roll  = 0
-        
-        text = 'Neigungswinkel: {0:+03d}°'.format(pitch)
-        text += ', Rollwinkel: {0:+03d}°'.format(roll)
-        text = text.replace('-0', '- ')
-        text = text.replace('+0', '+ ')
-        self.mw.set_value_normal(text)
+        self.mw.set_value_normal('Gewichtswert: ' + str(weight))
