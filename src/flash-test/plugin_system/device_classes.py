514ea6d33d5e9979096f757e1857f28c500d7d9d
Olaf Lüke <olaf@tinkerforge.com>
2015-08-28 09:35:06 +0200

    Add Analog In V2 and Laser Range Finder plugins

--- src/flash-test/plugin_system/device_classes.py
+++ src/flash-test/plugin_system/device_classes.py
@@ -1,6 +1,8 @@
 from .plugins.brick_imu_v2 import Plugin as brick_imu_v2_class
 from .plugins.bricklet_accelerometer import Plugin as bricklet_accelerometer_class
 from .plugins.bricklet_ambient_light_v2 import Plugin as bricklet_ambient_light_v2_class
+from .plugins.bricklet_analog_in_v2 import Plugin as bricklet_analog_in_v2_class
+from .plugins.bricklet_laser_range_finder import Plugin as bricklet_laser_range_finder_class
 from .plugins.bricklet_load_cell import Plugin as bricklet_load_cell_class
 from .plugins.bricklet_moisture import Plugin as bricklet_moisture_class
 from .plugins.bricklet_remote_switch import Plugin as bricklet_remote_switch_class
@@ -10,6 +12,8 @@ device_classes = [
     brick_imu_v2_class,
     bricklet_accelerometer_class,
     bricklet_ambient_light_v2_class,
+    bricklet_analog_in_v2_class,
+    bricklet_laser_range_finder_class,
     bricklet_load_cell_class,
     bricklet_moisture_class,
     bricklet_remote_switch_class,
---
+++ src/flash-test/plugin_system/plugins/bricklet_analog_in_v2.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+flash-test (Brick/Bricklet/Extension Flash and Test tool)
+Copyright (C) 2015 Olaf Lüke <olaf@tinkerforge.com>
+
+bricklet_analog_in_v2.py: Analog In 2.0 plugin
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+"""
+
+from PyQt4 import Qt, QtGui, QtCore
+
+from ..tinkerforge.bricklet_analog_in_v2 import BrickletAnalogInV2
+from ..bricklet_base import BrickletBase, get_bricklet_firmware_filename
+from ..callback_emulator import CallbackEmulator
+
+class Plugin(BrickletBase):
+    TODO_TEXT = u"""\
+1. Verbinde Analog In 2.0 Bricklet mit Port C
+2. Drücke "Flashen"
+3. Warte bis Master Brick neugestartet hat (Tool Status ändert sich wieder auf "Plugin gefunden")
+4. Überprüfe Wert:
+     * Spannung sollte mit eingespeister Spannung übereinstimmen
+5. Das Bricklet ist fertig, in kleine ESD-Tüte stecken, zuschweißen, Aufkleber aufkleben
+6. Gehe zu 1
+"""
+
+    def __init__(self, *args):
+        BrickletBase.__init__(self, *args)
+        self.cbe_voltage = None
+
+    def start(self, device_information):
+        BrickletBase.start(self, device_information)
+        
+        if device_information:
+            self.new_enum(device_information)
+
+    def stop(self):
+        if self.cbe_voltage:
+            self.cbe_voltage.set_period(0)
+
+    def get_device_identifier(self):
+        return BrickletAnalogInV2.DEVICE_IDENTIFIER
+    
+    def flash_clicked(self):
+        self.write_new_uid_to_bricklet()
+        self.write_plugin_to_bricklet(get_bricklet_firmware_filename('analog_in_v2'))
+        self.master_reset()
+        
+    def new_enum(self, device_information):
+        self.al = BrickletAnalogInV2(device_information.uid, self.get_ipcon())
+        self.cbe_voltage = CallbackEmulator(self.al.get_voltage,
+                                            self.cb_voltage)
+        self.cbe_voltage.set_period(100)
+
+        self.mw.set_tool_status_okay("Plugin gefunden")
+            
+    def cb_voltage(self, voltage):
+        self.mw.set_value_normal(str(round(voltage/1000.0, 2)) + ' V')
