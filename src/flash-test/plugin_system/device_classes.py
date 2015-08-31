baf5162cae16f87f428d04a96322f28fb0deb4bc
Olaf Lüke <olaf@tinkerforge.com>
2015-08-31 10:47:39 +0200

    Add Analog Out 2.0 plugin

--- src/flash-test/plugin_system/device_classes.py
+++ src/flash-test/plugin_system/device_classes.py
@@ -2,6 +2,7 @@ from .plugins.brick_imu_v2 import Plugin as brick_imu_v2_class
 from .plugins.bricklet_accelerometer import Plugin as bricklet_accelerometer_class
 from .plugins.bricklet_ambient_light_v2 import Plugin as bricklet_ambient_light_v2_class
 from .plugins.bricklet_analog_in_v2 import Plugin as bricklet_analog_in_v2_class
+from .plugins.bricklet_analog_out_v2 import Plugin as bricklet_analog_out_v2_class
 from .plugins.bricklet_laser_range_finder import Plugin as bricklet_laser_range_finder_class
 from .plugins.bricklet_load_cell import Plugin as bricklet_load_cell_class
 from .plugins.bricklet_moisture import Plugin as bricklet_moisture_class
@@ -13,6 +14,7 @@ device_classes = [
     bricklet_accelerometer_class,
     bricklet_ambient_light_v2_class,
     bricklet_analog_in_v2_class,
+    bricklet_analog_out_v2_class,
     bricklet_laser_range_finder_class,
     bricklet_load_cell_class,
     bricklet_moisture_class,
