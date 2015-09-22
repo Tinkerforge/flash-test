ba47b6f0a4d947b42ee05bb3619e122c9f6997b8
Olaf Lüke <olaf@tinkerforge.com>
2015-09-22 07:37:08 +0200

    Add Industrial Digitial In 4 Bricklet

--- src/flash-test/plugin_system/device_classes.py
+++ src/flash-test/plugin_system/device_classes.py
@@ -8,6 +8,7 @@ from .plugins.bricklet_barometer import Plugin as bricklet_barometer_class
 from .plugins.bricklet_dual_button import Plugin as bricklet_dual_button_class
 from .plugins.bricklet_dust_detector import Plugin as bricklet_dust_detector_class
 from .plugins.bricklet_industrial_analog_out import Plugin as bricklet_industrial_analog_out_class
+from .plugins.bricklet_industrial_digital_in_4 import Plugin as bricklet_industrial_digital_in_4_class
 from .plugins.bricklet_industrial_dual_analog_in import Plugin as bricklet_industrial_dual_analog_in_class
 from .plugins.bricklet_laser_range_finder import Plugin as bricklet_laser_range_finder_class
 from .plugins.bricklet_lcd_20x4 import Plugin as bricklet_lcd_20x4_class
@@ -28,6 +29,7 @@ device_classes = [
     bricklet_dual_button_class,
     bricklet_dust_detector_class,
     bricklet_industrial_analog_out_class,
+    bricklet_industrial_digital_in_4_class,
     bricklet_industrial_dual_analog_in_class,
     bricklet_laser_range_finder_class,
     bricklet_lcd_20x4_class,
---
+++ src/flash-test/plugin_system/plugins/bricklet_industrial_digital_in_4.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+flash-test (Brick/Bricklet/Extension Flash and Test tool)
+Copyright (C) 2015 Olaf Lüke <olaf@tinkerforge.com>
+
+bricklet_industrial_digital_in_4.py: Industrial Digital In 4 plugin
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+"""
+
+from PyQt4 import Qt, QtGui, QtCore
+
+from ..tinkerforge.bricklet_industrial_digital_in_4 import BrickletIndustrialDigitalIn4
+from ..bricklet_base import BrickletBase, get_bricklet_firmware_filename
+from ..callback_emulator import CallbackEmulator
+
+import time
+import math
+
+class Plugin(BrickletBase):
+    TODO_TEXT = u"""\
+1. Verbinde Industrial Digital In 4 Bricklet mit Port C
+2. Drücke "Flashen"
+3. Warte bis Master Brick neugestartet hat (Tool Status ändert sich wieder auf "Plugin gefunden")
+4. Drücke Knöpfe und überpüfe Werte.
+5. Das Bricklet ist fertig, mit grünem 8-Pol Stecker in ESD-Tüte stecken, zuschweißen, Aufkleber aufkleben
+6. Gehe zu 1
+"""
+
+    def __init__(self, *args):
+        BrickletBase.__init__(self, *args)
+        self.cbe_value = None
+        self.cbe_voltage1 = None
+        self.last_voltage = [0, 0]
+
+    def start(self, device_information):
+        BrickletBase.start(self, device_information)
+        
+        if device_information:
+            self.new_enum(device_information)
+
+    def get_device_identifier(self):
+        return BrickletIndustrialDigitalIn4.DEVICE_IDENTIFIER
+    
+    def flash_clicked(self):
+        self.flash_bricklet(get_bricklet_firmware_filename('industrial_digital_in_4'))
+        
+    def new_enum(self, device_information):
+        self.industrial_digital_in_4 = BrickletIndustrialDigitalIn4(device_information.uid, self.get_ipcon())
+        self.cbe_value = CallbackEmulator(self.industrial_digital_in_4.get_value, self.cb_value)
+        self.cbe_value.set_period(100)
+
+        self.show_device_information(device_information)
+            
+    def cb_value(self, value):
+        values = [value & 1, (value >> 1) & 1, (value >> 2) & 1, (value >> 3) & 1]
+        self.mw.set_value_normal('ch0: ' + str(values[0]) +  ', ch1: ' + str(values[1]) +  ', ch2: ' + str(values[2]) +  ', ch3: ' + str(values[3]))
