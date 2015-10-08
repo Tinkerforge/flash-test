0aa5116dea18bc91552facd17434faf992284075
Matthias Bolte <matthias@tinkerforge.com>
2015-10-08 10:13:24 +0200

    Turn download script into a full update script

--- download_firmwares.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-rm -rf download.tinkerforge.com firmwares
-wget --no-parent -r http://download.tinkerforge.com/firmwares/
-mv download.tinkerforge.com/firmwares firmwares
-rm -rf download.tinkerforge.com
