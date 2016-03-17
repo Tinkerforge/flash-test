e6177e0cd67dd88d5e10a64d04147de452cece6f
Matthias Bolte <matthias@tinkerforge.com>
2016-03-17 14:23:01 +0100

    Add Rotary Poti Bricket

--- src/flash-test/plugin_system/device_classes.py
+++ src/flash-test/plugin_system/device_classes.py
@@ -30,6 +30,7 @@ from .plugins.bricklet_ptc import Plugin as bricklet_ptc_class
 from .plugins.bricklet_real_time_clock import Plugin as bricklet_real_time_clock_class
 from .plugins.bricklet_remote_switch import Plugin as bricklet_remote_switch_class
 from .plugins.bricklet_rotary_encoder import Plugin as bricklet_rotary_encoder_class
+from .plugins.bricklet_rotary_poti import Plugin as bricklet_rotary_poti_class
 from .plugins.bricklet_rs232 import Plugin as bricklet_rs232_class
 from .plugins.bricklet_segment_display_4x7 import Plugin as bricklet_segment_display_4x7_class
 from .plugins.bricklet_solid_state_relay import Plugin as bricklet_solid_state_relay_class
@@ -74,6 +75,7 @@ device_classes = [
     bricklet_real_time_clock_class,
     bricklet_remote_switch_class,
     bricklet_rotary_encoder_class,
+    bricklet_rotary_poti_class,
     bricklet_rs232_class,
     bricklet_segment_display_4x7_class,
     bricklet_solid_state_relay_class,
--- src/flash-test/plugin_system/plugins/bricklet_rotary_poti.py
+++ src/flash-test/plugin_system/plugins/bricklet_rotary_poti.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 """
 flash-test (Brick/Bricklet/Extension Flash and Test tool)
-Copyright (C) 2015 Olaf Lüke <olaf@tinkerforge.com>
+Copyright (C) 2016 Matthias Bolte <matthias@tinkerforge.com>
 
-bricklet_rotary_encoder.py: Rotary Encoder plugin
+bricklet_rotary_poti.py: Rotary Poti plugin
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -23,7 +23,7 @@ Boston, MA 02111-1307, USA.
 
 from PyQt4 import Qt, QtGui, QtCore
 
-from ..tinkerforge.bricklet_rotary_encoder import BrickletRotaryEncoder
+from ..tinkerforge.bricklet_rotary_poti import BrickletRotaryPoti
 from ..bricklet_base import BrickletBase, get_bricklet_firmware_filename
 from ..callback_emulator import CallbackEmulator
 
@@ -31,25 +31,21 @@ import math
 
 class Plugin(BrickletBase):
     TODO_TEXT = u"""\
-1. Verbinde Rotary Encoder Bricklet mit Port C
+1. Verbinde Rotary Poti Bricklet mit Port C
 2. Drücke "Flashen"
 3. Warte bis Master Brick neugestartet hat (Tool Status ändert sich wieder auf "Plugin gefunden")
 4. Überprüfe Poti:
-     * Drehe bis < -5
-     * Drehe bis >  5
-     * Drücke Knopf
+     * Drehe bis -150
+     * Drehe bis +150
 5. Das Bricklet ist fertig, zusammen mit Knopf in normale ESD-Tüte stecken, zuschweißen, Aufkleber aufkleben
 6. Gehe zu 1
 """
 
     def __init__(self, *args):
         BrickletBase.__init__(self, *args)
-        self.cbe_count = None
-        self.led_is_on = False
+        self.cbe_position = None
         self.reached_negative = False
         self.reached_positive = False
-        self.pressed_count = 0
-        self.pressed_current = None
 
     def start(self, device_information):
         BrickletBase.start(self, device_information)
@@ -58,56 +54,44 @@ class Plugin(BrickletBase):
             self.new_enum(device_information)
 
     def stop(self):
-        if self.cbe_count:
-            self.cbe_count.set_period(0)
+        if self.cbe_position:
+            self.cbe_position.set_period(0)
 
     def get_device_identifier(self):
-        return BrickletRotaryEncoder.DEVICE_IDENTIFIER
+        return BrickletRotaryPoti.DEVICE_IDENTIFIER
     
     def flash_clicked(self):
-        self.flash_bricklet(get_bricklet_firmware_filename('rotary_encoder'))
+        self.flash_bricklet(get_bricklet_firmware_filename('rotary_poti'))
         
     def new_enum(self, device_information):
         self.reached_negative = False
         self.reached_positive = False
-        self.pressed_count = 0
-        self.pressed_current = None
         
-        self.rotary_encoder = BrickletRotaryEncoder(device_information.uid, self.get_ipcon())
-        self.pressed = self.rotary_encoder.is_pressed()
-        self.cbe_count = CallbackEmulator(lambda: self.rotary_encoder.get_count(False),
-                                          self.cb_count,
-                                          ignore_last_data=True)
-        self.cbe_count.set_period(100)
+        self.rotary_poti = BrickletRotaryPoti(device_information.uid, self.get_ipcon())
+        self.cbe_position = CallbackEmulator(self.rotary_poti.get_position,
+                                             self.cb_position,
+                                             ignore_last_data=True)
+        self.cbe_position.set_period(25)
 
         self.show_device_information(device_information)
             
-    def cb_count(self, count):
-        is_pressed = self.rotary_encoder.is_pressed()
-        if is_pressed != self.pressed_current:
-            self.pressed_current = is_pressed
-            self.pressed_count += 1
-            
-        pressed_done = self.pressed_count > 1
-        
-        if count < -5:
+    def cb_position(self, position):
+        if position <= -150:
             self.reached_negative = True
-        if count > 5:
+        if position >= 150:
             self.reached_positive = True
-            
-        pos = '\u25C7'
+
         neg = '\u25C7'
-        pre = '\u25C7'
-        if pressed_done:
-            pre = '\u25C6'
+        pos = '\u25C7'
         if self.reached_negative:
             neg = '\u25C6'
         if self.reached_positive:
             pos = '\u25C6'
             
-        if pressed_done and self.reached_negative and self.reached_positive:
+        if self.reached_negative and self.reached_positive:
             call = self.mw.set_value_okay
         else:
             call = self.mw.set_value_action
                 
-        call('Wert: ' + str(count) + ', gedrückt: ' + pre + ' | >5: ' + pos + ' | <5: ' + neg) 
+        call('Wert: ' + str(position) + ' | -150: ' + neg + ' | +150: ' + pos) 
+            
