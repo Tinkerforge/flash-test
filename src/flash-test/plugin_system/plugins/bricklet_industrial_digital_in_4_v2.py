52ba9923dbd23f40d9406b9a55a4dbd552d96ab0
Olaf Lüke <olaf@tinkerforge.com>
2018-06-05 11:44:50 +0200

    Add Industrial Digital In 4 2.0 test

---
+++ src/flash-test/plugin_system/plugins/bricklet_industrial_digital_in_4_v2.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+flash-test (Brick/Bricklet/Extension Flash and Test tool)
+Copyright (C) 2018 Olaf Lüke <olaf@tinkerforge.com>
+
+bricklet_industrial_digital_in_4_v2.py: Industrial Digital In 4 2.0 plugin
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+"""
+
+from PyQt4 import Qt, QtGui, QtCore
+
+from ..tinkerforge.bricklet_industrial_digital_in_4_v2 import BrickletIndustrialDigitalIn4V2
+from ..comcu_bricklet_base import CoMCUBrickletBase, get_bricklet_firmware_filename
+from ..callback_emulator import CallbackEmulator
+
+import time
+import math
+
+class Plugin(CoMCUBrickletBase):
+    TODO_TEXT = u"""\
+1. Verbinde Industrial Digital In 4 Bricklet 2.0 mit Port C
+2. Verbinde Schalter-Testadapter mit Industrial Digital In 4 Bricklet
+3. Drücke "Flashen"
+4. Warte bis Master Brick neugestartet hat (Tool Status ändert sich auf "Plugin gefunden")
+5. Schalte Schalter und überpüfe Werte und Status LEDs.
+6. Das Bricklet ist fertig, mit grünem 8-Pol Stecker in ESD-Tüte stecken, zuschweißen, Aufkleber aufkleben
+7. Gehe zu 1
+"""
+
+    def __init__(self, *args):
+        CoMCUBrickletBase.__init__(self, *args)
+        self.cbe_value = None
+        
+        self.last_values = None
+        self.changes = [0, 0, 0, 0]
+
+    def start(self, device_information):
+        CoMCUBrickletBase.start(self, device_information)
+        
+        if device_information:
+            self.new_enum(device_information)
+
+    def stop(self):
+        if self.cbe_value != None:
+            self.cbe_value.set_period(0)
+
+    def get_device_identifier(self):
+        return BrickletIndustrialDigitalIn4V2.DEVICE_IDENTIFIER
+    
+    def flash_clicked(self):
+        self.flash_bricklet(get_bricklet_firmware_filename(BrickletIndustrialDigitalIn4V2.DEVICE_URL_PART))
+        
+    def new_enum(self, device_information):
+        CoMCUBrickletBase.new_enum(self, device_information)
+
+        if self.cbe_value != None:
+            self.cbe_value.set_period(0)
+
+        self.changes = [0, 0, 0, 0]
+        self.last_values = None
+        self.industrial_digital_in_4_v2 = BrickletIndustrialDigitalIn4V2(device_information.uid, self.get_ipcon())
+        if self.industrial_digital_in_4_v2.get_bootloader_mode() != BrickletIndustrialDigitalIn4V2.BOOTLOADER_MODE_FIRMWARE:
+            return
+
+        self.cbe_value = CallbackEmulator(self.industrial_digital_in_4_v2.get_value, self.cb_value)
+        self.cbe_value.set_period(100)
+
+        self.show_device_information(device_information)
+            
+    def cb_value(self, values):
+        if self.last_values == None:
+            self.last_values = values
+        else:
+            for i in range(4):
+                if self.last_values[i] != values[i]:
+                    self.changes[i] += 1
+            self.last_values = values
+
+        show = []
+        for i in range(4):
+            if self.changes[i] > 1:
+                show.append('\u2611')
+            else:
+                show.append(1 if values[i] else 0)
+        
+        set_value = self.mw.set_value_action
+        if show[0] == '\u2611' and show[1] == '\u2611' and show[2] == '\u2611' and show[3] == '\u2611':
+            set_value =  self.mw.set_value_okay
+        
+        set_value("Werte: {0} {1} {2} {3} (0 = Masse, 1 = 24V, \u2611 = OK)".format(show[0], show[1], show[2], show[3]))
