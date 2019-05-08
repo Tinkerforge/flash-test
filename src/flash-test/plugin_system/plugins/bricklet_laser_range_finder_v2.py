05a15d8905abdd1abf6a631e5dc79c1fa5e6f5e6
Olaf Lüke <olaf@tinkerforge.com>
2019-05-08 16:03:45 +0200

    Add Laser Range Finder Bricklet 2.0 calibration

--- src/flash-test/plugin_system/plugins/bricklet_laser_range_finder_v2.py
+++ src/flash-test/plugin_system/plugins/bricklet_laser_range_finder_v2.py
@@ -25,19 +25,24 @@ from ..tinkerforge.bricklet_laser_range_finder_v2 import BrickletLaserRangeFinde
 from ..comcu_bricklet_base import CoMCUBrickletBase, get_bricklet_firmware_filename
 from ..callback_emulator import CallbackEmulator
 
+import time
+
 class Plugin(CoMCUBrickletBase):
     TODO_TEXT = u"""\
 1. Verbinde Laser Range Finder Bricklet 2.0 mit Port C
-2. Drücke "Flashen"
-3. Warte bis Master Brick neugestartet hat (Tool Status ändert sich auf "Plugin gefunden")
-4. Überprüfe Wert (z.B. ~2m zur Decke)
-5. Das Bricklet ist fertig, in normale ESD-Tüte stecken, zuschweißen, Aufkleber aufkleben
-6. Gehe zu 1
+2. Stelle Bricklet fest auf tisch (guckt zur Decke), nicht wackeln während Kalibrierung! 
+3. Drücke "Flashen"
+4. Warte bis Master Brick neugestartet hat (Tool Status ändert sich auf "Plugin gefunden")
+5. Bricklet Kalibriert sich (dauert ca. 1s)
+6. Überprüfe Wert (ca. 180cm zur Decke)
+7. Das Bricklet ist fertig, in normale ESD-Tüte stecken, zuschweißen, Aufkleber aufkleben
+8. Gehe zu 1
 """
 
     def __init__(self, *args):
         CoMCUBrickletBase.__init__(self, *args)
         self.cbe_distance = None
+        self.offset = 0
 
     def start(self, device_information):
         CoMCUBrickletBase.start(self, device_information)
@@ -65,12 +70,26 @@ class Plugin(CoMCUBrickletBase):
         if self.laser_range_finder.get_bootloader_mode() != BrickletLaserRangeFinderV2.BOOTLOADER_MODE_FIRMWARE:
             return
 
+
+        self.mw.set_value_normal('Kalibriere...')
+        self.laser_range_finder.set_enable(True)
+        time.sleep(0.25)
+        self.laser_range_finder.set_offset_calibration(0)
+        self.laser_range_finder.set_configuration(128, True, 1, 10)
+        time.sleep(0.25)
+
+        velocity_sum = 0
+        for i in range(10):
+            time.sleep(0.1)
+            velocity_sum += self.laser_range_finder.get_velocity()
+
+        self.offset = int(round(velocity_sum/100, 0))
+        self.laser_range_finder.set_offset_calibration(self.offset)
+
         self.cbe_distance = CallbackEmulator(self.laser_range_finder.get_distance, self.cb_distance)
         self.cbe_distance.set_period(100)
-
-        self.laser_range_finder.enable_laser() # FIXME: Use correct new API
 
         self.show_device_information(device_information)
 
     def cb_distance(self, distance):
-        self.mw.set_value_normal('Entfernung: ' + str(distance) + ' cm')
+        self.mw.set_value_normal('Offset: {0}, Entfernung: {1} cm'.format(self.offset, distance))
