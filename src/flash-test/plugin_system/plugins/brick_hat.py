f72cc54c9bd90720a14f096d900b0ef20a6587bb
Olaf Lüke <olaf@tinkerforge.com>
2019-06-26 15:27:39 +0200

    Improve HAT Brick instructions

--- src/flash-test/plugin_system/plugins/brick_hat.py
+++ src/flash-test/plugin_system/plugins/brick_hat.py
@@ -37,7 +37,8 @@ class Plugin(CoMCUBrickletBase):
 3. Warte bis Master Brick neugestartet hat (Tool Status ändert sich auf "Plugin gefunden")
 4. Lange Kondensatorpinne abschneiden und Gummifuss aufkleben
 5. Das HAT Brick muss jetzt mit RPi und RPI-Test-Image getestet werden
-6. Gehe zu 1
+6. 2-Pol Stecker beilegen, eintüten, einschweißen
+7. Gehe zu 1
 """
 
     def __init__(self, *args):
