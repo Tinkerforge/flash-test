ec6e87bbb8e28061fc849264207e415f253c53c0
Matthias Bolte <matthias@tinkerforge.com>
2023-01-10 11:05:09 +0100

    isolator: Longer power-off duration to deal with extra capacity

--- src/flash-test/plugin_system/plugins/bricklet_isolator.py
+++ src/flash-test/plugin_system/plugins/bricklet_isolator.py
@@ -56,7 +56,7 @@ class Plugin(CoMCUBrickletBase):
         return BrickletIsolator.DEVICE_IDENTIFIER
 
     def flash_clicked(self):
-        self.flash_bricklet(get_bricklet_firmware_filename(BrickletIsolator.DEVICE_URL_PART))
+        self.flash_bricklet(get_bricklet_firmware_filename(BrickletIsolator.DEVICE_URL_PART), power_off_duration=0.75)
 
     def new_enum(self, device_information):
         CoMCUBrickletBase.new_enum(self, device_information)
